dd05c16c6484e3b54baeb931e84f13af537cb1a1
Pascal Honoré <pascal@alseyn.net>
2019-11-01 15:58:18 +0000

    Using 4 images per Grid

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { FontCSS, TdCSS, TableCSS, ImageCSS } from "../css";
 import { palette } from "@guardian/src-foundations";
+import React from "react";
+import { FontCSS, ImageCSS, TableCSS, TdCSS } from "../css";
 
 const imgStyle: ImageCSS = {
     outline: "none",
@@ -41,7 +41,7 @@ const innerTableStyle: TableCSS = {
     width: "100%",
     position: "relative",
     height: "56px",
-}
+};
 
 const metaWrapperStyle: TdCSS = {
     padding: "3px 10px 5px"
@@ -63,81 +63,81 @@ const bottomPaddingStyle: TdCSS = {
     paddingBottom: "20px"
 };
 
-interface Props {
+interface ColumnsProps {
+    headline1: string;
+    webURL1: string;
+    imageURL1?: string;
+    imageAlt1?: string;
+    headline2: string;
+    webURL2: string;
+    imageURL2?: string;
+    imageAlt2?: string;
+}
+
+const GridCell: React.FC<{
     headline: string;
-    byline: string;
     webURL: string;
     imageURL?: string;
     imageAlt?: string;
-}
-
-export const Columns: React.FC<Props> = ({
+}> = ({
     headline,
     webURL,
     imageURL,
     imageAlt
 }) => (
+    <table style={tableStyle}>
+        <tr valign="top">
+            <td>
+                <a href={webURL}>
+                    <img
+                        width="294"
+                        style={imgStyle}
+                        alt={imageAlt}
+                        src={imageURL}
+                    />
+                </a>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td className="h-pad" style={metaWrapperStyle}>
+                <a style={linkStyle} href={webURL}>
+                    <span className="col-h-sm" style={headlineStyle}>{headline}</span>
+                </a>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td className="col-pad" style={bottomPaddingStyle}></td>
+        </tr>
+    </table>
+);
+
+export const Columns: React.FC<ColumnsProps> = ({
+    headline1,
+    webURL1,
+    imageURL1,
+    imageAlt1,
+    headline2,
+    webURL2,
+    imageURL2,
+    imageAlt2,
+}) => (
     <table style={mainTableStyle}>
         <tr>
             <td style={tdStyle}>
                 <table style={innerTableStyle}>
-                {imageURL && (
-                    <tr valign="top">
+                <tr valign="top">
                         <td style={tdColumnStyle}>
-                            <table style={tableStyle}>
-                                <tr valign="top">
-                                    <td>
-                                        <a href={webURL}>
-                                            <img
-                                                width="294"
-                                                style={imgStyle}
-                                                alt={imageAlt}
-                                                src={imageURL}
-                                            />
-                                        </a>
-                                    </td>
-                                </tr>
-                                <tr valign="top">
-                                    <td className="h-pad" style={metaWrapperStyle}>
-                                        <a style={linkStyle} href={webURL}>
-                                            <span className="col-h-sm" style={headlineStyle}>{headline}</span>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <tr valign="top">
-                                    <td className="col-pad" style={bottomPaddingStyle}></td>
-                                </tr>
-                            </table>
+                            {imageURL1 && (
+                                <GridCell headline={headline1} webURL={webURL1} imageURL={imageURL1} imageAlt={imageAlt1} />
+                            )}
                         </td>
                         <td className="gutter">&nbsp;</td>
                         <td style={tdColumnStyle}>
-                            <table style={tableStyle}>
-                                <tr valign="top">
-                                    <td>
-                                        <a href={webURL}>
-                                            <img
-                                                width="294"
-                                                style={imgStyle}
-                                                alt={imageAlt}
-                                                src={imageURL}
-                                            />
-                                        </a>
-                                    </td>
-                                </tr>
-                                <tr valign="top">
-                                    <td className="h-pad" style={metaWrapperStyle}>
-                                        <a style={linkStyle} href={webURL}>
-                                            <span className="col-h-sm" style={headlineStyle}>{headline}</span>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <tr valign="top">
-                                    <td className="col-pad" style={bottomPaddingStyle}></td>
-                                </tr>
-                            </table>
+                            {imageURL2 && (
+                                <GridCell headline={headline2} webURL={webURL2} imageURL={imageURL2} imageAlt={imageAlt2} />
+                            )}
                         </td>
                     </tr>
-                )}
                 </table>
             </td>
         </tr>
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -8,25 +8,48 @@ export const Grid: React.FC<{
     collection: ICollection;
     salt: string;
 }> = ({ collection, salt }) => {
-    const col = collection.backfill.slice(2, 6).map(content => {
-        const image =
-            content.properties.maybeContent.trail.trailPicture.allImages[0];
-        const formattedImage = formatImage(image.url, salt);
+    const content1 = collection.backfill[2];
+    const content2 = collection.backfill[3];
+    const content3 = collection.backfill[4];
+    const content4 = collection.backfill[5];
 
-        return (
-            <>
-                <Columns
-                    imageURL={formattedImage}
-                    imageAlt={image.fields.altText}
-                    headline={content.properties.webTitle}
-                    byline={content.properties.byline}
-                    webURL={content.properties.webUrl}
-                    key={content.properties.webUrl}
-                />
-                <Padding px={10} />
-            </>
-        );
-    });
+    const image1 = content1.properties.maybeContent.trail.trailPicture.allImages[0];
+    const formattedImage1 = formatImage(image1.url, salt);
 
-    return <>{col}</>;
+    const image2 = content2.properties.maybeContent.trail.trailPicture.allImages[0];
+    const formattedImage2 = formatImage(image2.url, salt);
+
+    const image3 = content3.properties.maybeContent.trail.trailPicture.allImages[0];
+    const formattedImage3 = formatImage(image3.url, salt);
+
+    const image4 = content4.properties.maybeContent.trail.trailPicture.allImages[0];
+    const formattedImage4 = formatImage(image4.url, salt);
+
+    return (
+        <>
+            <Columns
+                headline1={content1.properties.webTitle}
+                webURL1={content1.properties.webUrl}
+                imageURL1={formattedImage1}
+                imageAlt1={image1.fields.altText}
+                headline2={content2.properties.webTitle}
+                webURL2={content2.properties.webUrl}
+                imageURL2={formattedImage2}
+                imageAlt2={image2.fields.altText}
+            />
+            <Padding px={10} />
+            <Columns
+                headline1={content3.properties.webTitle}
+                webURL1={content3.properties.webUrl}
+                imageURL1={formattedImage3}
+                imageAlt1={image3.fields.altText}
+                headline2={content4.properties.webTitle}
+                webURL2={content4.properties.webUrl}
+                imageURL2={formattedImage4}
+                imageAlt2={image4.fields.altText}
+            />
+            <Padding px={10} />
+        </>
+    );
+
 };
