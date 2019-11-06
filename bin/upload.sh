73e861ba492bb03d57940b2bf01fde2cfa55dd76
Nicolas Long <nicolas.long@theguardian.com>
2019-11-06 15:22:15 +0000

    Add tmp script to write responses to s3 for inital a/b test

---
+++ bin/upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf tmp/ # clear old files
+
+wget "http://localhost:3030/film-today.json" --quiet --timeout 1 -O /dev/null
+if [ $? -ne 0 ]
+then
+    echo "Service not available: run yarn dev first."
+    exit 1
+fi
+
+wget --quiet localhost:3030/film-today.json --directory-prefix=tmp/
+wget --quiet localhost:3030/film-today/text.json --directory-prefix=tmp/
+
+aws s3 cp --profile=frontend --acl=public-read --recursive tmp/ s3://aws-frontend-emails-test/
