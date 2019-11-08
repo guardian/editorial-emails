832fca8bd19dd0083109abcbdf442c1906d88502
Krystian Kwiatkowski <krystian.kwiatkowski.freelancer@guardian.co.uk>
2019-11-08 12:03:00 +0000

    Table layout helper created and added in to the banner component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import { ImageCSS, TableCSS } from "../css";
-
-const tableStyle: TableCSS = {
-    borderSpacing: 0,
-    borderCollapse: "collapse",
-    width: "100%"
-};
+import { ImageCSS } from "../css";
+import { Table } from "../layout/Table";
 
 const imgStyle: ImageCSS = {
     outline: "none",
@@ -14,20 +9,16 @@ const imgStyle: ImageCSS = {
     clear: "both",
     display: "block",
     border: "0",
-    width: "100%",
+    width: "100%"
 };
 
 export const Banner: React.FC<{}> = () => (
-    <table style={tableStyle}>
-        <tr>
-            <td>
-                <img
-                    width="600"
-                    src="https://assets.guim.co.uk/images/email/banners/0dbd7be9345b28a8678baaae474e6548/film-today.png"
-                    alt="Film Today"
-                    style={imgStyle}
-                ></img>
-            </td>
-        </tr>
-    </table>
+    <Table>
+        <img
+            width="600"
+            src="https://assets.guim.co.uk/images/email/banners/0dbd7be9345b28a8678baaae474e6548/film-today.png"
+            alt="Film Today"
+            style={imgStyle}
+        ></img>
+    </Table>
 );
