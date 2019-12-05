a186eba861232bf434d1778a29e997ec518bce62
Krystian Kwiatkowski <krystian.kwiatkowski.freelancer@guardian.co.uk>
2019-12-05 17:42:31 +0000

    Banner adjusted and Top collection added in

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,6 +8,8 @@ export const Banner: React.FC<{ frontId: string }> = ({ frontId }) => {
             "https://assets.guim.co.uk/images/email/banners/5ddb54b70715242bc85e071bd14f66e8/opinion.png",
         "email/media-briefing":
             "https://assets.guim.co.uk/images/email/banners/7c27c2af5c0e7ab17516908fe012bc13/media-briefing.png",
+        "email/sport-au":
+            "https://assets.guim.co.uk/images/email/banners/907e4c059bce8ffc82260ea5e140759f/australia-sports.png",
         default:
             "https://assets.guim.co.uk/images/email/banners/0dbd7be9345b28a8678baaae474e6548/film-today.png"
     };
