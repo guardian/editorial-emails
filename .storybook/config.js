863e7928df556e6d7805d8e3494706947732ec90
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2019-12-10 15:47:33 +0000

    Deletes storybook files that shouldn't be here

--- .storybook/config.js
+++
@@ -1,6 +0,0 @@
-import { configure } from "@storybook/react";
-
-const stories = require.context("../src", true, /\.stories\.js$/);
-console.log("=== Stories: ", stories);
-
-configure(stories, module);
