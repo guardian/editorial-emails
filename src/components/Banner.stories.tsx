91213270b8d298e14bc7c5d41e392fd183fa8b3d
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2019-12-10 17:44:29 +0000

    Stories for Banner and Footer

--- src/components/Banner.stories.tsx
+++ src/components/Banner.stories.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Footer } from "./Footer";
+import { Banner } from "./Banner";
 import { withKnobs, text } from "@storybook/addon-knobs";
 import { Center } from "../layout/Center";
 import { TableRowCell } from "../layout/Table";
 
 export default {
-    component: Footer,
-    title: "Components/Footer",
+    component: Banner,
+    title: "Components/Banner",
     decorators: [withKnobs]
 };
 
 export const defaultStory = () => (
     <Center>
         <TableRowCell>
-            <Footer
+            <Banner
                 title={text("title", "Film Today")}
                 frontId={text("frontId", "email/film-today")}
             />
@@ -23,14 +23,28 @@ export const defaultStory = () => (
 
 defaultStory.story = { name: "Film Today" };
 
-export const opinionStory = () => (
+export const mediaBriefingStory = () => (
     <Center>
         <TableRowCell>
-            <Footer
+            <Banner
+                title={text("title", "Media Briefing")}
+                frontId={text("frontId", "email/media-briefing")}
+            />
+        </TableRowCell>
+    </Center>
+);
+
+mediaBriefingStory.story = { name: "Media Briefing" };
+
+export const opinionBriefingStory = () => (
+    <Center>
+        <TableRowCell>
+            <Banner
                 title={text("title", "The Best of Guardian Opinion")}
                 frontId={text("frontId", "email/opinion")}
             />
         </TableRowCell>
     </Center>
 );
-opinionStory.story = { name: "The Best of Guardian Opinion" };
+
+opinionBriefingStory.story = { name: "The Best of Guardian Opinion" };
