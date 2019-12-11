f3f9b10b81b8ed5e812c101ac37a926a9fdc5e8f
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2019-12-11 17:51:08 +0000

    Adds new Opinion Variants B & C; renames old variants

--- src/fronts/opinion/Opinion.tsx
+++ src/fronts/opinion/Opinion.tsx
@@ -3,6 +3,8 @@ import { Collection as ICollection } from "../../api";
 import { Collections } from "../../components/Collections";
 import { Collections as CollectionsVariantB } from "./variantB/Collections";
 import { Collections as CollectionsVariantC } from "./variantC/Collections";
+import { Collections as CollectionsVariantY } from "./variantY/Collections";
+import { Collections as CollectionsVariantZ } from "./variantZ/Collections";
 
 export const Opinion: React.FC<{
     frontId: string;
@@ -10,7 +12,27 @@ export const Opinion: React.FC<{
     salt: string;
     variant?: string;
 }> = ({ frontId, collections, salt, variant }) => {
-    if (variant === "c") {
+    if (variant === "z") {
+        // NOT IN USE
+        // FKA VARIANT C
+        return (
+            <CollectionsVariantZ
+                frontId={frontId}
+                collections={collections}
+                salt={salt}
+            ></CollectionsVariantZ>
+        );
+    } else if (variant === "y") {
+        // NOT IN USE
+        // FKA VARIANT B
+        return (
+            <CollectionsVariantY
+                frontId={frontId}
+                collections={collections}
+                salt={salt}
+            ></CollectionsVariantY>
+        );
+    } else if (variant === "c") {
         return (
             <CollectionsVariantC
                 frontId={frontId}
---
+++ src/fronts/opinion/variantZ/Collections.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Collection as ICollection } from "../../../api";
+import { TableRowCell } from "../../../layout/Table";
+import { CommentCollection } from "./components/CommentCollection";
+import { EditorialCollection } from "./components/EditorialCollection";
+import { MediaCollection } from "./components/MediaCollection";
+import { LinkCollection } from "./components/LinkCollection";
+import { DefaultCollection } from "./components/DefaultCollection";
+import { getDesignType } from "../../../utils/getDesignType";
+
+export const Collections: React.FC<{
+    frontId: string;
+    collections: ICollection[];
+    salt: string;
+}> = ({ frontId, collections, salt }) => {
+    const renderedCollections = collections.map(collection => {
+        const content = [].concat(collection.backfill, collection.curated); // TODO support curated too
+        const designType = getDesignType(content);
+
+        switch (designType) {
+            case "editorial":
+                return (
+                    <EditorialCollection collection={collection} salt={salt} />
+                );
+            case "comment":
+                return (
+                    <CommentCollection
+                        frontId={frontId}
+                        collection={collection}
+                        salt={salt}
+                    />
+                );
+            case "media":
+                return <MediaCollection collection={collection} salt={salt} />;
+            case "link":
+                // Ignore 'Guardian Subscribe/Masterclasses' collection without using 'display name'
+                // Look at combination of content type (curated/backfill),
+                // content length and collection type
+                if (
+                    collection.curated.length === 1 &&
+                    collection.collectionType === "medium"
+                ) {
+                    return null;
+                }
+
+                return <LinkCollection collection={collection} salt={salt} />;
+            case "default":
+                return (
+                    <DefaultCollection collection={collection} salt={salt} />
+                );
+        }
+    });
+
+    return <TableRowCell>{renderedCollections}</TableRowCell>;
+};
