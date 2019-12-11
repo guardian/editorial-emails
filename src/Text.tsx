a130b1848d0ac21428ada2af61cb475193ba0da3
Nicolas Long <nicolas.long@theguardian.com>
2019-12-11 14:14:30 +0000

    Incomplete stash

--- src/Text.tsx
+++ src/Text.tsx
@@ -1,26 +1,33 @@
 import { Front } from "./api";
 import { getPageTitle } from "./Email";
 import { disclaimer } from "./components/Footer";
+import { Collection, Content } from "./api";
 
 // The text-only email version
 export const Text = (front: Front): string => {
+    const getContent = (col: Collection): Content[] => {
+        return [].concat(col.backfill, col.curated);
+    };
+
     const pageTitle = getPageTitle(front);
-    const collection = front.collections[0];
 
     return `${front.seoData.webTitle} | The Guardian
 
 
-${collection.displayName}
+${front.collections
+    .map(collection => {
+        return `${collection.displayName}
 
-
-${collection.backfill
+${getContent(collection)
     .map(
         content =>
             `${content.header.headline}\n${content.properties.webUrl}##braze_utm##`
     )
     .join("\n\n\n")}
 
-
+`;
+    })
+    .join("\n")}
 Read online: https://www.theguardian.com/${front.id}?##braze_utm##.
 
 
