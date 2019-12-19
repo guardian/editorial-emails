79873f97a04053d3c54c1d9dd684d8b9d0952555
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2019-12-19 14:46:04 +0000

    Disable Free Text Braze links

--- src/components/TextLink.tsx
+++ src/components/TextLink.tsx
@@ -8,11 +8,11 @@ export const TextLink: React.FC<{
     const brazeParameter = "?##braze_utm##";
 
     // Append Braze parameter if it's a Guardian URL
-    const isGuardianLink = linkTo.includes("theguardian.com/");
-    const linkHref = isGuardianLink ? linkTo + brazeParameter : linkTo;
+    // const isGuardianLink = linkTo.includes("theguardian.com/");
+    // const linkHref = isGuardianLink ? linkTo + brazeParameter : linkTo;
 
     return (
-        <a href={linkHref} style={{ color: palette.neutral[7] }}>
+        <a href={linkTo} style={{ color: palette.neutral[7] }}>
             {linkText}
         </a>
     );
