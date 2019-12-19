bdd1c53f4c69fb1098c35ee54af62c206c477d23
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2019-12-19 17:22:52 +0000

    Deletes old code

--- src/components/FreeTextLink.tsx
+++ src/components/FreeTextLink.tsx
@@ -10,12 +10,6 @@ export const FreeTextLink: React.FC<{
     linkTo: string;
     linkText: string;
 }> = ({ linkTo, linkText }) => {
-    // const brazeParameter = "?##braze_utm##";
-
-    // Append Braze parameter if it's a Guardian URL
-    // const isGuardianLink = linkTo.includes("theguardian.com/");
-    // const linkHref = isGuardianLink ? linkTo + brazeParameter : linkTo;
-
     return (
         <a href={linkTo} style={freeTextStyles}>
             {linkText}
