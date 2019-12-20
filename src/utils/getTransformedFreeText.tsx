3b564f3d72d7e852849a2c242008cacabe7c0a38
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2019-12-20 13:19:57 +0000

    Better fix; using mock text

--- src/utils/getTransformedFreeText.tsx
+++ src/utils/getTransformedFreeText.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { renderToStaticMarkup } from "react-dom/server";
+import { palette } from "@guardian/src-foundations";
 import ReactHtmlParser from "react-html-parser";
-import { FreeTextLink } from "../components/FreeTextLink";
 import { Image } from "../components/Image";
 
 export const getTransformedFreeText = (freeText: string): string => {
@@ -13,65 +13,73 @@ export const getTransformedFreeText = (freeText: string): string => {
     return renderToStaticMarkup(parsedText);
 };
 
+export const FreeTextLink: React.FC<{
+    linkTo: string;
+    children: any;
+}> = ({ linkTo, children }) => {
+    return (
+        <a
+            href={linkTo}
+            style={{ color: palette.neutral[7], textDecoration: "underline" }}
+            dangerouslySetInnerHTML={{
+                __html: children
+            }}
+        />
+    );
+};
+
 export const FreeTextBold: React.FC<{
-    text: string;
-}> = ({ text }) => <strong>{text}</strong>;
+    children: any;
+}> = ({ children }) => (
+    <strong
+        dangerouslySetInnerHTML={{
+            __html: children
+        }}
+    />
+);
 
 export const FreeTextItalic: React.FC<{
-    text: string;
-}> = ({ text }) => <em>{text}</em>;
+    children: any;
+}> = ({ children }) => (
+    <em
+        dangerouslySetInnerHTML={{
+            __html: children
+        }}
+    />
+);
 
 const getNodeText = (node: any): any => {
-    console.log("========> NODE");
-    console.log(node);
-
     const nodeText = node.children.map((child: any) => {
         if (child.type === "text") {
             return child.data;
         } else if (child.type === "tag") {
             if (child.name === "strong") {
                 return renderToStaticMarkup(
-                    <FreeTextBold text={getNodeText(child)} />
+                    <FreeTextBold>{getNodeText(child)}</FreeTextBold>
                 );
             }
             if (child.name === "em") {
                 return renderToStaticMarkup(
-                    <FreeTextItalic text={getNodeText(child)} />
+                    <FreeTextItalic>{getNodeText(child)}</FreeTextItalic>
                 );
             }
-            // return `<${child.name}>${getNodeText(child)}</${child.name}>`;
         }
+
         return getNodeText(child);
     });
-
-    // console.log("=== NODE TEXT");
-    // console.log(nodeText);
 
     return nodeText.join("");
 };
 
-const transform = (node: any): React.ReactElement | null => {
+const transform = (node: any, index: number): React.ReactElement | null => {
     if (node.type === "tag" && node.name === "a" && node.attribs.href) {
-        // Only replace <a> tag with FreeTextLink if we can successfully
-        // read the link text from inside the <a> tag
-        // const linkTo = node.attribs.href;
-        // const linkText =
-        //     node.children && node.children[0] ? node.children[0].data : null;
+        const linkChildren = getNodeText(node);
 
-        //console.log("======= LINK");
-        //console.log(node);
-
-        // let linkTest = node.children.map(child =>
-        //     child.type === "text" ? child.data : child.children[0].data
-        // );
-
-        const linkText = getNodeText(node);
-        console.log("=== LINK TEXT:");
-        console.log(linkText);
-
-        if (linkText) {
+        if (linkChildren) {
             return (
-                <FreeTextLink linkTo={node.attribs.href} linkText={linkText} />
+                <FreeTextLink linkTo={node.attribs.href}>
+                    {linkChildren}
+                </FreeTextLink>
             );
         }
     }
